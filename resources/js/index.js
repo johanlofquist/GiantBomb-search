5240bac686ee2cd7a11b7d9970e147b5fb101f25
Johan Löfquist <johan.lofquist@gmail.com>
2023-11-02 21:42:01 +0100

    Addid api fetch

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,16 +1,47 @@
 const inputFieldEl = document.querySelector("#input__field");
 const goBtn = document.querySelector("#go__btn");
+const mainGameContainer = document.querySelector("#main__game__container");
 
 goBtn.addEventListener("click", getData);
 
 async function getData() {
+  mainGameContainer.innerHTML = "";
   let link =
     "https://corsproxy.io/?https://www.giantbomb.com/api/search/?api_key=3fe3d85dfed048a636b145709d6402eae063a7c0&format=json&query=%22" +
     inputFieldEl.value +
     "%22&resources=game";
   const response = await fetch(link, {
-    cors: "no-cors"
-})
+    cors: "no-cors",
+  });
   const data = await response.json();
   console.log(data);
+
+  for (let i = 0; i < data.results.length; i++) {
+    let gameContainer = document.createElement("div");
+    gameContainer.classList = "game__container";
+    let gameImg = document.createElement("img");
+    gameImg.classList = "game__img";
+    let gameTextContainer = document.createElement("div");
+    gameTextContainer.classList = "game__text__container";
+    let gameName = document.createElement("h2");
+    gameName.classList = "game__name";
+    let gameDescription = document.createElement("p");
+    gameDescription.classList = "game__description";
+    let gameRelease = document.createElement("p");
+    gameRelease.classList = "game__release";
+    let gamePlatforms = document.createElement("p");
+    gamePlatforms.classList = "game__platforms";
+
+    gameImg.src = data.results[i].image.original_url;
+    gameName.innerText = data.results[i].name
+
+    mainGameContainer.append(gameContainer);
+    gameContainer.append(gameImg, gameTextContainer);
+    gameTextContainer.append(
+      gameName,
+      gameDescription,
+      gameRelease,
+      gamePlatforms
+    );
+  }
 }
